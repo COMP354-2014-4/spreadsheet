87f8406bd63b7191856ef080ac6044bc082151a2
NormanMirotchnick <norman@redhot5.com>
2014-01-12 21:31:41 -0500

    -- Added some structural comments
    -- started placing layout of methods to write

--- src/spreadsheet.java
+++ src/spreadsheet.java
@@ -3,7 +3,19 @@ public class spreadsheet {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+    
+    //variables
+    
+    //default constructor
+    
+    //accessors
+    
+    //mutators
+    
+    //additional methods
 
   }
+  
+  //static methods
 
 }
