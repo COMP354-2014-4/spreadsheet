5d03bfb712b2b5ce6a0c79581f8d82a65f357897
Norman Mirotchnick <norman@redhot5.com>
2014-01-27 22:57:44 -0500

    -- updated ormatting and modifying some questionable code
    -- need to pass the test!

--- src-test/CellTest.java
+++ src-test/CellTest.java
@@ -41,131 +41,202 @@ public class CellTest {
   }
 
   
+  /*************************/
   /**** Testing Methods ****/
+  /*************************/
   
-  //Testing default constructor - value in a cell
+  /*** Waiting for implementation ***/
+  /**********************************/
+  
+  /**
+   * Testing display cell
+   */
+  public void testDisplayCell(){
+    fail("Not yet implemented");
+  }
+  
+  /**
+   * Testing if value is valid
+   */
+  public void testvalidateValue(){
+    fail("Not yet implemented");
+  }
+
+  
+
+  /*** Exception Tests ***/
+  /***********************/
+
+  
+  
+  /*** Equals Tests ***/
+  /********************/
+
+  /**
+   * Testing default constructor - value in a cell
+   */
   @Test
   public void testCellValue1() {
-	  Cell cell = new Cell("A",1,test_Grid);
-	  assertEquals("Initial cell value", 0.0, cell.getEvaluatedValue(),ACCEPTED_ERROR);
+    Cell cell = new Cell("A",1,test_Grid);
+    assertEquals("Initial cell value", 0.0, cell.getEvaluatedValue(),ACCEPTED_ERROR);
   }
   
-  //Testing default constructor - formula in a cell
+  /**
+   * Testing default constructor - formula in a cell
+   */
   @Test
   public void testCellFormula() {
-	  Cell cell = new Cell("A",1,test_Grid);
-	  assertEquals("Initial cell formula", 0.0, cell.evaluate(),ACCEPTED_ERROR);
+    Cell cell = new Cell("A",1,test_Grid);
+    assertEquals("Initial cell formula", 0.0, cell.evaluate(),ACCEPTED_ERROR);
   }
- 
+  
+  /**
+   * Test if value is stored in cell
+   */
+  @Test
+  public void testGetValue(){
+    Cell cell  = new Cell("B", 1, test_Grid);
+    cell.setValue("28.0");
+    assertEquals("Cell set value", "28.0", cell.getValue());
+  }
+  
+  /**
+   * Testing if the new value set in cell works
+   */
+  @Test
+  public void testSetValue(){
+    Cell cell = new Cell("B",2,test_Grid);
+    cell.setValue("29.0");
+    cell.setValue("30.0");
+    assertEquals("Cell new set value", "30.0", cell.getValue());
+  }
+  
+  @Test
+  /***
+   * Test if value in cell is not a number
+   */
+  public void testCellValueEquals(){
+    Cell cell = new Cell("B", 4, test_Grid);
+    cell.setValue("bb");
+    assertEquals("Cell invalid value", "bb", cell.getValue());
+  }
+  
   /*
   //Getters And Setters
   //Testing getRow() method
   //Testing boundaries
   @Test 
   public void testGetRow() {
-	  Cell cell = new Cell("A", 1, test_Grid);	//Creating a new cell
-	  assertEquals("Getting cell's row", "A",cell.getRow());	//Should return an int 
+    Cell cell = new Cell("A", 1, test_Grid);  //Creating a new cell
+    assertEquals("Getting cell's row", "A",cell.getRow());  //Should return an int 
   }
   @Test
   public void testGetRow2() {
-	  Cell cell = new Cell("A", 10, test_Grid);	//Creating a new cell
-	  assertEquals("Getting cell's row", "A",cell.getRow());
+    Cell cell = new Cell("A", 10, test_Grid); //Creating a new cell
+    assertEquals("Getting cell's row", "A",cell.getRow());
   }
   
   //Testing valid rows
   @Test
   public void testGetRow3() {
-	  Cell cell = new Cell("A", 5, test_Grid);	//Creating a new cell
-	  assertEquals("Getting cell's row", "A",cell.getRow());
+    Cell cell = new Cell("A", 5, test_Grid);  //Creating a new cell
+    assertEquals("Getting cell's row", "A",cell.getRow());
   }
   @Test
   public void testGetRow4() {
-	  Cell cell = new Cell("A", 7, test_Grid);	//Creating a new cell
-	  assertEquals("Getting cell's row", "A",cell.getRow());
+    Cell cell = new Cell("A", 7, test_Grid);  //Creating a new cell
+    assertEquals("Getting cell's row", "A",cell.getRow());
   }
-  
-  //Testing invalid rows
-  @Test
-  public void testGetRow5() {
-	  Cell cell = new Cell("A", -1, test_Grid);	//Creating a new cell
-	  assertNull("Getting cell's row", cell.getRow());
-  }
-  @Test
-  public void testGetRow6() {
-	  Cell cell = new Cell("A", 50, test_Grid);	//Creating a new cell
-	  assertNull("Getting cell's row", cell.getRow());
-  }
-  
+  */
+
+  /*
   //Testing GetCol() method
   @Test
   //Testing boundaries
   public void testGetCol1(){
-	  Cell cell = new Cell("A",3,test_Grid);	//Creating a new cell
-	  assertEquals("Getting cell's column", 3, cell.getCol());
+    Cell cell = new Cell("A",3,test_Grid);  //Creating a new cell
+    assertEquals("Getting cell's column", 3, cell.getCol());
   }
   @Test
   public void testGetCol2(){
-	  Cell cell = new Cell("J",3,test_Grid);	//Creating a new cell
-	  assertEquals("Getting cell's column", 3, cell.getCol());
+    Cell cell = new Cell("J",3,test_Grid);  //Creating a new cell
+    assertEquals("Getting cell's column", 3, cell.getCol());
   }
  
   //Testing valid columns
   @Test
   public void testGetCol3(){
-	  Cell cell = new Cell("C",3,test_Grid);	//Creating a new cell
-	  assertEquals("Getting cell's column", 3, cell.getCol());
+    Cell cell = new Cell("C",3,test_Grid);  //Creating a new cell
+    assertEquals("Getting cell's column", 3, cell.getCol());
   }
   @Test
   public void testGetCol4(){
-	  Cell cell = new Cell("D",3,test_Grid);	//Creating a new cell
-	  assertEquals("Getting cell's column", 3, cell.getCol());
-  }
-  
-  //Testing invalid columns
-  @Test
-  public void testGetCol5(){
-	  Cell cell = new Cell("Z",3,test_Grid);	//Creating a new cell
-	  assertNull("Getting cell's column", cell.getCol());
-  }
-  @Test
-  public void testGetCol6(){
-	  Cell cell = new Cell("M",3,test_Grid);	//Creating a new cell
-	  assertNull("Getting cell's column", cell.getCol());
+    Cell cell = new Cell("D",3,test_Grid);  //Creating a new cell
+    assertEquals("Getting cell's column", 3, cell.getCol());
   }
   */
   
-  //Test if value is stored in cell
+
+
+  /*** Equals with tolerance Tests ***/
+  /***********************************/
+  
+  
+
+  /*** True Tests ***/
+  /******************/
+
+
+  
+  /*** False Tests ***/
+  /*******************/
+
+  
+
+  /*** Null Tests ***/
+  /******************/
+
+  /*
+  //Testing invalid rows
   @Test
-  public void testGetValue(){
-	  Cell cell  = new Cell("B", 1, test_Grid);
-	  cell.setValue("28.0");
-	  assertEquals("Cell set value", "28.0", cell.getValue());
+  public void testGetRow5() {
+    Cell cell = new Cell("A", -1, test_Grid); //Creating a new cell
+    assertNull("Getting cell's row", cell.getRow());
   }
-  
-  //Testing if the new value set in cell works
   @Test
-  public void testSetValue(){
-	  Cell cell = new Cell("B",2,test_Grid);
-	  cell.setValue("29.0");
-	  cell.setValue("30.0");
-	  assertEquals("Cell new set value", "30.0", cell.getValue());
+  public void testGetRow6() {
+    Cell cell = new Cell("A", 50, test_Grid); //Creating a new cell
+    assertNull("Getting cell's row", cell.getRow());
   }
+  */
   
+  /*
+  //Testing invalid columns
   @Test
-  //Test if value in cell is not a number
-  public void testCellValueException(){
-	  Cell cell = new Cell("B", 4, test_Grid);
-	  cell.setValue("bb");
-	  assertEquals("Cell invalid value", "bb", cell.getValue());
+  public void testGetCol5(){
+    Cell cell = new Cell("Z",3,test_Grid);  //Creating a new cell
+    assertNull("Getting cell's column", cell.getCol());
   }
+  @Test
+  public void testGetCol6(){
+    Cell cell = new Cell("M",3,test_Grid);  //Creating a new cell
+    assertNull("Getting cell's column", cell.getCol());
+  }
+  */
   
-  //Testing display cell
-  public void testDiaplyCell(){
-	  fail("Not yet implemented");
-  }
   
-  //Testing if value is valid
-  public void testvalidateValue(){
-	  fail("Not yet implemented");
-  }
+  
+  /*** Not Null Tests ***/
+  /**********************/
+  
+
+
+  /*** Same Tests ***/
+  /******************/
+  
+
+
+  /*** Not Same Tests ***/
+  /**********************/
+
 }
