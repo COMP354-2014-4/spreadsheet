018793045a1295ec12b6e4d498fea2224c6f0f84
Jayanti Rani <jayanti_rani@hotmail.fr>
2014-03-02 23:03:48 -0500

    --- classes for testing paste and load ---

---
+++ src-test/testLoad.java
@@ -0,0 +1,140 @@
+import static org.junit.Assert.*;
+
+
+import spreadsheet.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import org.junit.rules.TemporaryFolder; // create temp files and be removed after the test
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Jayanti Rani
+ *
+ */
+public class testLoad {
+
+	// variables
+	private static Robot robot;
+	private static SSGUI gui;
+	private static Grid grid;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		robot = new Robot();
+		grid = new Grid ();
+		gui = new SSGUI(grid);
+		}
+	
+@Rule
+ public TemporaryFolder folder = new TemporaryFolder();
+	
+	/**
+	 * method for simulating mouse click
+	 */
+	public static void click () {
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.delay(2000); // Click 2 seconds
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	/**
+	 * create temp file to check the load function
+	 * @throws IOException
+	 */
+	 @Test
+	  public void testUsingTempFolder() throws IOException {
+	    File createdFile = folder.newFile("myfilefile.sav");
+	    assertTrue(createdFile.exists());
+	  }
+	
+
+	 /**
+	  * method to input a file name using robot
+	  * @param s   String name of the file
+	  */
+	public static void inputString(String s){
+        char[] charArray = s.toCharArray();
+       robot.waitForIdle();
+        for(char c : charArray){
+                if (Character.isUpperCase(c)) {
+            robot.keyPress(KeyEvent.VK_SHIFT);
+        }
+                robot.keyPress(Character.toUpperCase(c));
+                robot.keyRelease(Character.toUpperCase(c));
+
+        if (Character.isUpperCase(c)) {
+                robot.keyRelease(KeyEvent.VK_SHIFT);
+        }
+        robot.waitForIdle();
+        }
+       
+	}
+
+
+	/**
+	 * load test for testing from direct location for Load
+	*/
+	@Test
+	public void testLoad1() {
+		  robot.mouseMove(gui.btnLoad.getLocationOnScreen().x , gui.btnLoad.getLocationOnScreen().y);
+		 click();
+		 inputString("myfilefile.sav");
+		 robot.delay(2000); 
+		 robot.keyPress(KeyEvent.VK_ENTER);
+		 robot.keyRelease(KeyEvent.VK_ENTER);
+		 robot.delay(2000); 
+	}
+	 
+	/**
+	 * load test for testing from File --> Load
+	 */
+	
+	@Test
+	public void testLoad2() {
+		robot.mouseMove(gui.mnuFile.getLocationOnScreen().x , gui.mnuFile.getLocationOnScreen().y);
+		click();
+		robot.mouseMove(gui.mniLoad.getLocationOnScreen().x , gui.mniLoad.getLocationOnScreen().y);
+		click();
+		 inputString("myfilefile.sav");
+		 robot.delay(2000); 
+		 robot.keyPress(KeyEvent.VK_ENTER);
+		 robot.keyRelease(KeyEvent.VK_ENTER);
+		 robot.delay(2000); 
+		 
+	}
+
+}
---
+++ src-test/testPaste.java
@@ -0,0 +1,182 @@
+import static org.junit.Assert.*;
+import spreadsheet.*;
+
+
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Jayanti Rani
+ *
+ */
+public class testPaste {
+
+	// variables
+		private static Robot robot;
+		private static SSGUI gui;
+		private static Grid grid;
+		private static Rectangle rect;
+		private Point pCell, pTable, mMouse;
+		
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		robot = new Robot();
+		grid = new Grid ();
+		gui = new SSGUI(grid);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		gui.clipBoard = "0";
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		gui.clipBoard = "0";
+	}
+	
+	/**
+	 * method for simulating mouse click
+	 */
+	public static void click () {
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.delay(2000); // Click 2 seconds
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+
+	public static void inputString(String s){
+        char[] charArray = s.toCharArray();
+       robot.waitForIdle();
+        for(char c : charArray){
+                if (Character.isUpperCase(c)) {
+            robot.keyPress(KeyEvent.VK_SHIFT);
+        }
+                robot.keyPress(Character.toUpperCase(c));
+                robot.keyRelease(Character.toUpperCase(c));
+
+        if (Character.isUpperCase(c)) {
+                robot.keyRelease(KeyEvent.VK_SHIFT);
+        }
+        robot.waitForIdle();
+        }
+	}
+	
+	/**
+	 * method to select a given cell
+	 */
+	
+	public void SelectCellAndWrite (int a, int b){
+		
+		rect= gui.tblGrid.getCellRect(a,b,true);
+		pCell = rect.getLocation();
+		pTable = gui.tblGrid.getLocationOnScreen();
+		mMouse = new Point(pCell.x + 5 + pTable.x , pCell.y + 5 + pTable.y);
+		robot.mouseMove(mMouse.x, mMouse.y);
+		click();
+
+	}
+	
+	/**
+	 *  test to check if Paste is working from the direct location
+	 */
+	@Test
+	public void testPaste1() {
+		SelectCellAndWrite(0,3);
+		gui.clipBoard = "123";
+		robot.mouseMove(gui.btnPaste.getLocationOnScreen().x , gui.btnPaste.getLocationOnScreen().y);
+		 click(); 
+		 robot.delay(1000);
+		 double input = 123.0;
+		 assertEquals(input, gui.tblGrid.getValueAt(0, 3));
+	}
+	
+	/**
+	 *  test to check if Paste is working from the edit --> paste
+	 */
+
+	@Test
+	public void testPaste2() {
+		SelectCellAndWrite(1,3);
+		gui.clipBoard = "99";
+		
+		robot.mouseMove(gui.mnuEdit.getLocationOnScreen().x , gui.mnuEdit.getLocationOnScreen().y);
+		click();
+		robot.mouseMove(gui.mniPaste.getLocationOnScreen().x , gui.mniPaste.getLocationOnScreen().y);
+		 click(); 
+		 robot.delay(1000);
+		 double input = 99.0;
+		 assertEquals(input, gui.tblGrid.getValueAt(1, 3));
+	}
+	
+	/**
+	 *  test to check whether Paste works with an empty clipboard 
+	 *  Paste from toolbar
+	 */
+	@Test
+	public void testPaste3() {
+		SelectCellAndWrite(2,3);
+		robot.mouseMove(gui.btnPaste.getLocationOnScreen().x , gui.btnPaste.getLocationOnScreen().y);
+		click(); 
+		robot.delay(1000);
+	}
+	
+	/**
+	 *  test to check whether Paste works with an empty clipboard 
+	 *  Paste from edit --> paste
+	 */
+	@Test
+	public void testPaste4() {
+		SelectCellAndWrite(5,3);
+		robot.mouseMove(gui.mnuEdit.getLocationOnScreen().x , gui.mnuEdit.getLocationOnScreen().y);
+		click();
+		robot.mouseMove(gui.mniPaste.getLocationOnScreen().x , gui.mniPaste.getLocationOnScreen().y);
+		 click(); 
+		 robot.delay(2000);
+		 boolean input = true;
+		 assertEquals(input, gui.clipBoard.equals("0"));
+	}
+	
+	/**
+	 *  test to check if Paste is working for formula
+	 */
+	@Test
+	public void testPaste5() {
+		SelectCellAndWrite(3,3);
+		gui.clipBoard = "=123*2";
+		robot.mouseMove(gui.btnPaste.getLocationOnScreen().x , gui.btnPaste.getLocationOnScreen().y);
+		 click(); 
+		 robot.delay(1000);
+		 double input = 246.0;
+		 assertEquals(input, gui.tblGrid.getValueAt(3, 3));
+	}
+	
+}
