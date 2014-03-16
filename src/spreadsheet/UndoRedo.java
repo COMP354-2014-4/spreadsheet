539ffc4ac6c29d999a88251da661f0f496532c17
n_mirot <n_mirot@FETTUCCINE.ENCS.concordia.ca>
2014-03-16 15:16:20 -0400

    -- added UndoRedo class to model subsystem
    -- this implements undo and redo features in spreadsheet

---
+++ src/spreadsheet/UndoRedo.java
@@ -0,0 +1,13 @@
+package spreadsheet;
+
+/**
+ * 
+ * @author n_mirot
+ * This class implements the Undo and Redo functionality for the 
+ * Spreadsheet application
+ */
+
+public class UndoRedo {
+	private int stackSize = 25;
+	
+}
